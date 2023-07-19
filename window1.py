d98b88902003ee50f2121d72e89c55f4668489bb
Gene014 <calmaeugene06@gmail.com>
2023-07-19 19:09:50 +0800

    no button inserted

--- window1.py
+++ window1.py
@@ -23,5 +23,29 @@ background = canvas.create_image(
     400.0, 250.0,
     image=background_img)
 
+canvas.create_text(
+    414.5, 241.5,
+    text="In compliance with the Data Privacy Act of 2012\nand its Implementing Rules and Regulations, we\nexecute reasonable and appropriate securit\nmeasures for the protection of personal data\nthat we collect. \n\nPress Yes if you want to continue, otherwise,\npress No if you want to terminate the application.",
+    fill="#fff",
+    justify="center",
+    font=('"Arial Narrow" 16 bold'))
+
+canvas.create_text(
+    415.0, 105.5,
+    text="Contact Tracing Form",
+    fill="#fff",
+    font=("Inter-Bold", int(24.0)))
+
+img0 = PhotoImage(file=f"img0.png")
+b0 = Button(
+    image=img0,
+    borderwidth=0,
+    highlightthickness=0,
+    relief="flat")
+b0.place(
+    x=439, y=345,
+    width=131,
+    height=65)
+
 window.resizable(False, False)
 window.mainloop()
