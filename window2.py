bd257974fe09fd244f0bcae4ce33e4cdebe08218
Gene014 <calmaeugene06@gmail.com>
2023-07-19 21:09:41 +0800

    generated name text field, alr working

--- window2.py
+++ window2.py
@@ -25,7 +25,31 @@ class App:
         TitleBlock["text"] = "Contact Tracing Form"
         TitleBlock.place(x=280, y=10, width=265, height=30)
 
+        # NAME TEXT FIELD
+        def on_entry_focus_in1(event):
+            if user_name.get() == placeholder_text1:
+                user_name.delete(0, tk.END)
+                user_name.configure(show="")
+                user_name.configure(fg="#333333")
 
+        def on_entry_focus_out1(event):
+            if user_name.get() == "":
+                user_name.insert(0, placeholder_text1)
+                user_name.configure(fg="gray")
+
+        user_name = tk.Entry(root)
+        user_name["borderwidth"] = "1px"
+        ft = tkFont.Font(family='Times', size=10)
+        user_name["font"] = ft
+        user_name["fg"] = "gray"
+        user_name["justify"] = "left"
+        user_name["text"] = "Name"
+        user_name.place(x=30, y=60, width=220, height=30)
+
+        placeholder_text1 = "Enter Full name"
+        user_name.insert(0, placeholder_text1)
+        user_name.bind("<FocusIn>", on_entry_focus_in1)
+        user_name.bind("<FocusOut>", on_entry_focus_out1)
 
 if __name__ == "__main__":
     root = tk.Tk()
