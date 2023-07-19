adaf0100a83a9e1641c0bb4e9aa82f1c7630859a
Gene014 <calmaeugene06@gmail.com>
2023-07-19 19:00:27 +0800

    inserting the needed images and background for themes and button

---
+++ window1.py
@@ -0,0 +1,27 @@
+# Input the generated front from the design application you created thru paper
+# Generate the first and second window
+
+from tkinter import *
+import window2
+
+window = Tk()
+
+window.geometry("800x500")
+window.configure(bg="#ffffff")
+canvas = Canvas(
+    window,
+    bg="#ffffff",
+    height=500,
+    width=800,
+    bd=0,
+    highlightthickness=0,
+    relief="ridge")
+canvas.place(x=0, y=0)
+
+background_img = PhotoImage(file=f"contact tracing\\background.png")
+background = canvas.create_image(
+    400.0, 250.0,
+    image=background_img)
+
+window.resizable(False, False)
+window.mainloop()
