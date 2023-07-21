ed2582a5d1d84386ce7ef411947a5eb6b66a1362
Gene014 <calmaeugene06@gmail.com>
2023-07-21 20:09:48 +0800

    finalizing window 1

--- window1.py
+++ window1.py
@@ -4,19 +4,25 @@
 from tkinter import *
 import window2
 
-def btn_clickedNo():
+def btn_clickedYes():
     window.destroy()
     root = Tk()
     app = window2.App(root)
     root.mainloop()
 
-
-def btn_clickedYes():
+def btn_clickedNo():
     window.destroy()
 
 window = Tk()
 
-window.geometry("800x500")
+width = 800
+height = 500
+screenwidth = window.winfo_screenwidth()
+screenheight = window.winfo_screenheight()
+alignstr = '%dx%d+%d+%d' % (width, height,
+                            (screenwidth - width) / 2, (screenheight - height) / 2)
+window.geometry(alignstr)
+window.resizable(width=False, height=False)
 window.configure(bg="#ffffff")
 window.title('Data Act')
 canvas = Canvas(
@@ -35,41 +41,37 @@ background = canvas.create_image(
     image=background_img)
 
 canvas.create_text(
-    414.5, 241.5,
-    text="In compliance with the Data Privacy Act of 2012\nand its Implementing Rules and Regulations, we\nexecute reasonable and appropriate securit\nmeasures for the protection of personal data\nthat we collect. \n\nPress Yes if you want to continue, otherwise,\npress No if you want to terminate the application.",
-    fill="#fff",
-    justify="center",
-    font=('"Arial Narrow" 16 bold'))
-
-canvas.create_text(
-    415.0, 105.5,
+    415.0, 60,
     text="Contact Tracing Form",
     fill="#fff",
-    font=("Inter-Bold", int(24.0)))
+    font=('"Inter" 32 bold'))
 
-img0 = PhotoImage(file=f"img0.png")
+canvas.create_text(
+    414.5, 225,
+    text="In compliance with the Data Privacy Act of 2012 and its Implementing Rules and Regulations, we execute reasonable and appropriate security measures for the protection of personal data\nthat we collect. \n\nPress Yes if you want to continue, otherwise, press No if you want to terminate the application.",
+    fill="#fff",
+    width=480,
+    justify="center",
+    font=('"Inter" 16 bold'))
+
+
+img1 = PhotoImage(file=f"img1.png")
 b0 = Button(
-    image=img0,
+    image=img1,
     borderwidth=0,
     highlightthickness=0,
     command=btn_clickedYes,
     relief="flat")
-b0.place(
-    x=439, y=345,
-    width=131,
-    height=65)
+b0.place(x=240, y=360)
 
-img1 = PhotoImage(file=f"img1.png")
+img0 = PhotoImage(file=f"img0.png")
 b1 = Button(
-    image=img1,
+    image=img0,
     borderwidth=0,
     highlightthickness=0,
     command=btn_clickedNo,    
     relief="flat")
-b1.place(
-    x=240, y=345,
-    width=131,
-    height=65)
+b1.place(x=439, y=360,)
 
 window.resizable(False, False)
 window.mainloop()
